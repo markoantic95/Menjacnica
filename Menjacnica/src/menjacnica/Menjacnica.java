135719905c79e74a9228e01fcea4e07f8155e506
markoantic95 <markoantic95@gmail.com>
2016-03-28 16:43:26 +0200

    Implementirana do kraja klasa koja implementira interfejs MenjacnicaInterfejs.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -4,12 +4,24 @@ import java.util.GregorianCalendar;
 
 public class Menjacnica {
 	public void dodajKurs(Valuta valuta, Kurs kurs){
-		
+		if(valuta == null || kurs == null){
+			throw new RuntimeException("Greska");
+		}
+		valuta.getKursevi().add(kurs);
 	}
 	public void obrisiKurs(Valuta valuta, GregorianCalendar datum){
-		
+		for (int i = 0; i < valuta.getKursevi().size(); i++) {
+			if(valuta.getKursevi().get(i).getDatum().compareTo(datum)==0){
+				valuta.getKursevi().remove(i);
+			}
+		}
 	}
 	public Kurs prondajiKurs(Valuta valuta, GregorianCalendar datum){
+		for (int i = 0; i < valuta.getKursevi().size(); i++) {
+			if(valuta.getKursevi().get(i).getDatum().compareTo(datum)==0){
+				return valuta.getKursevi().get(i);
+				}
+			}
 		return null;
 	}
 }
