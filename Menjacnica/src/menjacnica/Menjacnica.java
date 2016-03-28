ff3b3e24eb18e7389b25812a7b7a336ddc7dd254
markoantic95 <markoantic95@gmail.com>
2016-03-28 16:38:40 +0200

    Napravljena klasa koja implementira interfejs MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,15 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Menjacnica {
+	public void dodajKurs(Valuta valuta, Kurs kurs){
+		
+	}
+	public void obrisiKurs(Valuta valuta, GregorianCalendar datum){
+		
+	}
+	public Kurs prondajiKurs(Valuta valuta, GregorianCalendar datum){
+		return null;
+	}
+}
