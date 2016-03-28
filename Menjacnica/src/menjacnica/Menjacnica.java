2d0fee21963981a9c1db488a2fd2ee2e5da47098
markoantic95 <markoantic95@gmail.com>
2016-03-28 16:57:54 +0200

    Napravljena je alternativna metoda za dodavanje kursa valute

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -4,7 +4,10 @@ import java.util.GregorianCalendar;
 
 public class Menjacnica {
 	public void dodajKurs(Valuta valuta, Kurs kurs){
+		if(kurs == null || valuta == null) throw new RuntimeException("Greska");
 		
+		
+		valuta.getKursevi().add(kurs);
 	}
 	public void obrisiKurs(Valuta valuta, GregorianCalendar datum){
 		
